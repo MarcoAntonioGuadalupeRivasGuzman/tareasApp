5cdf36a5e98c8f89abd75e95af421bd74b4e8292
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-15 17:38:33 -0600

    add a code to show all task in the taskpage

--- src/pages/tareaspage.jsx
+++ src/pages/tareaspage.jsx
@@ -4,10 +4,11 @@ import { getTaskRequest } from "../api/tareasApi";
 function TareasPage(){
 
     const [tareas, setTareas]=useState([]);
-    
+
     const loadTask= async () =>{
         const response= await getTaskRequest();
         console.log(response);
+        setTareas(response.data);
     }
 
     useEffect(()=>{
@@ -17,7 +18,16 @@ function TareasPage(){
     return(
         <>
         <h1>Tareas</h1>
-
+        {
+            tareas.map(tarea=>(
+                <div>
+                    <h2>{tarea.title}</h2>
+                    <p>{tarea.description}</p>
+                    <span>{tarea.done == 1 ? "done": "not done" }</span>
+                    <span>{tarea.createAt}</span>
+                </div>
+            ))
+        }
         </>
     );
 }
