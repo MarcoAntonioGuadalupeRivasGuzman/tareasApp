d6723258c3862ce585361b289dbc797b47b557a0
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-15 22:35:26 -0600

    add a context provider to use a context in the componeents

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import TareasPage from './pages/tareaspage';
 import TareasForm from './pages/tareasform';
 import NotFound from './pages/notfound';
 import Navbar from './components/navbar';
+import { TareaContextProvider } from './context/tareasContext';
+
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<TareasPage />} /> 
-      <Route path='/new' element={<TareasForm />} />
-      <Route path='*' element={<NotFound/>} />
-    </Routes>
+      <TareaContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<TareasPage />} />
+          <Route path='/new' element={<TareasForm />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </TareaContextProvider>
     </>
   );
 }
