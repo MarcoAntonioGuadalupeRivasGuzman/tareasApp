ba0322a1a25607a4fc79cb43c000ab5e30d27e16
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-15 20:10:02 -0600

    implements a component to show a tareas in card

--- src/pages/tareaspage.jsx
+++ src/pages/tareaspage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getTaskRequest } from "../api/tareasApi";
+import TareaCard from "../components/tareaCard";
 
 function TareasPage(){
 
@@ -20,14 +21,7 @@ function TareasPage(){
         <h1>Tareas</h1>
         {
             tareas.map(tarea=>(
-                <div key={tarea.id}>
-                    <h2>{tarea.title}</h2>
-                    <p>{tarea.description}</p>
-                    <span>{tarea.done == 1 ? "done": "not done" }</span>
-                    <span>{tarea.createAt}</span>
-                    <button>Delete</button>
-                    <button>Edit</button>
-                </div>
+                <TareaCard tarea={tarea} key={tarea.id} />
             ))
         }
         </>
