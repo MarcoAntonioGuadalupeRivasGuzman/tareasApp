d7964f156fcf43e446b7d187c73546d1daa9afbc
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-09-17 19:32:25 -0600

    add a new function on click for the buttom update

--- src/components/tareaCard.jsx
+++ src/components/tareaCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { deleteTaskRequest } from "../api/tareasApi";
 import { useTarea } from "../context/tareasContext";
+import { useNavigate } from "react-router-dom";
 
 function TareaCard({ tarea }) {
 
     const {onDelete} = useTarea();
+    const navigate=useNavigate();
 
     return (
         <div>
@@ -13,7 +15,7 @@ function TareaCard({ tarea }) {
             <span>{tarea.done == 1 ? "done" : "not done"}</span>
             <span>{tarea.createAt}</span>
             <button onClick={()=>onDelete(tarea.id)}>Delete</button>
-            <button>Edit</button>
+            <button onClick={()=>navigate(`/edit/${tarea.id}`)}>Edit</button>
         </div>
     );
 }
